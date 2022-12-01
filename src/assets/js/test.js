9dc78cf1dd19eb2585a3769684aac172b7b17477
GustavsIljuconoks <gustavs.iljuconoks@gmail.com>
2022-12-01 23:03:38 +0200

    Created Login page

---
+++ src/assets/js/test.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+
+// Toogles side bar when pressed on "hamburger"
+const offcanvasElementList = document.querySelectorAll('.offcanvas');
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl));
